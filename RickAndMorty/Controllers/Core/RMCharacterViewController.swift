3419cd3b4476874f65108778de380527bfb2d2c2
Jaxongir Kayumov <jaxongirkayumov2004@gmail.com>
2023-01-02 18:27:52 +0500

    Create API request

--- RickAndMorty/Controllers/Core/RMCharacterViewController.swift
+++ RickAndMorty/Controllers/Core/RMCharacterViewController.swift
@@ -14,7 +14,7 @@ final class RMCharacterViewController: UIViewController {
         super.viewDidLoad()
 
         view.backgroundColor = .systemBackground
-        title = "Characters"
+        title = "Characters" 
     }
     
 }
