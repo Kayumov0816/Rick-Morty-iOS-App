bc9a0e4af1f4fbf9ecdab13c88f6868a851d582c
Jaxongir Kayumov <jaxongirkayumov2004@gmail.com>
2023-01-04 17:53:04 +0500

    Character Cell

--- RickAndMorty/ViewModels/RMCharacterListViewViewModel.swift
+++ RickAndMorty/ViewModels/RMCharacterListViewViewModel.swift
@@ -7,13 +7,12 @@
 
 import UIKit
 
-final class CharacterListViewViewModel: NSObject {
+final class RMCharacterListViewViewModel: NSObject {
     func fetchCharacters() {
         RMService.shared.execute(.listCharactersRequests, expecting: RMGetAllCharactersResponse.self) { result in
             switch result {
             case .success(let model):
-                print("Total: "+String(model.info.pages))
-                print("Page result count: "+String(model.results.count))
+                print("URL: "+String(model.results.first?.image ?? "No Image"))
             case .failure(let error):
                 print(String(describing: error))
             }
@@ -21,14 +20,22 @@ final class CharacterListViewViewModel: NSObject {
     }
 }
 
-extension CharacterListViewViewModel: UICollectionViewDataSource, UICollectionViewDelegate, UICollectionViewDelegateFlowLayout {
+extension RMCharacterListViewViewModel: UICollectionViewDataSource, UICollectionViewDelegate, UICollectionViewDelegateFlowLayout {
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return 20
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath)
-        cell.backgroundColor = .systemGreen
+        guard let cell = collectionView.dequeueReusableCell(
+            withReuseIdentifier: RMCharacterCollectionViewCell.cellIdentifier,
+            for: indexPath
+        ) as? RMCharacterCollectionViewCell else {
+            fatalError("Unsupported Cell")
+        }
+        let viewModel = RMCharacterCollectionViewCellViewModel(characterName: "Jaxangir",
+                                                               characterStatus: .alive,
+                                                               characterImageUrl: nil)
+        cell.configure(with: viewModel)
         return cell
     }
     
